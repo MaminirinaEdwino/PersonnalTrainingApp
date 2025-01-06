6e5eb91bdd41a79dfb362f654e2ec91fc5046a3f
MaminirinaEdwino <edwinomaminirina@gmail.com>
2025-01-06 17:46:06 +0300

    modif titre

--- src/components/titre.jsx
+++ src/components/titre.jsx
@@ -1,5 +1,5 @@
 export default function Titre() {
-    return <header className="w-full text-4xl text-end p-5 shadow-2xl bg-white drop-shadow-lg font-bold">
+    return <header className="w-full text-4xl text-end p-5 shadow-2xl bg-white drop-shadow-lg font-bold sticky top-0 z-20">
         TrainingAPP
     </header>
 }
